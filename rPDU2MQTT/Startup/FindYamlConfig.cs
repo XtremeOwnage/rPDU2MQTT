3f7f123e8c5a9ef278dd916b7e86f06f1eb4adcb
XtremeOwnageDotCom <5262735+XtremeOwnageDotCom@users.noreply.github.com>
2024-08-29 21:51:24 -0500

    Refactored application startup, and configuration discovery.

---
+++ rPDU2MQTT/Startup/FindYamlConfig.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using rPDU2MQTT.Classes;
+using System.Runtime.InteropServices;
+using YamlDotNet.Serialization;
+
+namespace rPDU2MQTT.Startup;
+/// <summary>
+/// This class, validates a YAML configuration exists, and returns the path.
+/// </summary>
+internal class FindYamlConfig
+{
+    public static string Find()
+    {
+        Console.WriteLine("Attempting to locate configuration file.");
+
+        // Before starting- we need to validate a configuration file exists.
+        string[] SearchPaths = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) switch
+        {
+            true => [Environment.CurrentDirectory],
+            false => ["/config", Environment.CurrentDirectory],
+        };
+
+        string[] FileNames = ["config"];
+
+        string[] YamlExtensions = [".yaml", ".yml"];
+
+        var combinations = from path in SearchPaths
+                           from file in FileNames
+                           from extension in YamlExtensions
+                           select Path.Combine(path, $"{file}{extension}");
+
+        foreach (var file in combinations)
+        {
+            if (File.Exists(file))
+                return file;
+        }
+
+        /// At this point, we cannot find a configuration.
+        /// Print an error to the console, and lets add a sleep / delay
+        /// before throwing the exception.
+        /// 
+
+        Console.WriteLine("Unable to locate config.yaml. Paths searched:");
+        foreach (var file in combinations)
+            Console.WriteLine($"\t{file}");
+        Console.WriteLine("Restarting in 15 seconds.");
+
+        System.Threading.Thread.Sleep(TimeSpan.FromSeconds(15));
+
+        throw new Exception("Unable to locate config.yaml");
+    }
+
+    public static Config GetConfig()
+    {
+        var ds = new DeserializerBuilder();
+        IDeserializer s = ds.Build();
+
+        using var stream = File.OpenRead(Find());
+        using var sr = new StreamReader(stream);
+
+        var cfg = s.Deserialize<Config>(sr);
+
+        return cfg;
+    }
+}
---
+++ rPDU2MQTT/Startup/ServiceConfiguration.cs
@@ -0,0 +1,73 @@
+﻿using HiveMQtt.Client;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using rPDU2MQTT.Classes;
+using rPDU2MQTT.Services;
+
+namespace rPDU2MQTT.Startup;
+
+public static class ServiceConfiguration
+{
+    public static void Configure(HostBuilderContext context, IServiceCollection services)
+    {
+        // While- we can request services when building dependancies-
+        // Need the configuration DURING service collection initilization- 
+        // Because it determiens which hosted services we want to add.
+        Config cfg = FindYamlConfig.GetConfig() ?? throw new Exception("Unable to load configuration");
+
+        // Bind Configuration
+        services.AddSingleton(cfg);
+
+        bool mqttEnabled = !string.IsNullOrEmpty(cfg.MQTT?.Host);
+        if (!mqttEnabled)
+            Console.WriteLine("Error: No MQTT configuration found. MQTT-related services will not be started.");
+
+        if (mqttEnabled)
+            // Bind IHiveMQClient
+            services.AddSingleton<IHiveMQClient, HiveMQClient>((sp) =>
+            {
+                if (cfg.MQTT is null)
+                    throw new Exception("Configuration: MQTT is required.");
+
+                var mqttBuilder = new HiveMQClientOptionsBuilder()
+                    .WithBroker(cfg.MQTT?.Host ?? throw new Exception("Configuration: MQTT.Host is required."))
+                    .WithPort(cfg.MQTT.Port)
+                    .WithClientId(cfg.MQTT.ClientID ?? "rpdu2mqtt");
+
+                if (cfg.MQTT.Credentials?.Username is not null)
+                    mqttBuilder.WithUserName(cfg.MQTT.Credentials.Username);
+
+                if (cfg.MQTT.Credentials?.Password is not null)
+                    mqttBuilder.WithPassword(cfg.MQTT.Credentials.Password);
+
+                // Return new client, with options applied.
+                return new HiveMQClient(mqttBuilder.Build());
+            });
+
+        // Create HttpClient for PDU.
+        services.AddHttpClient<PDU>(client =>
+        {
+            client.BaseAddress = new Uri(cfg.PDU.Url);
+            client.Timeout = TimeSpan.FromSeconds(cfg.PDU.Timeout);
+        });
+
+        //Configure Services
+        services.AddSingleton<PDU>();
+
+
+        if (mqttEnabled)
+        {
+            services.AddSingleton<MQTTServiceDependancies>();
+
+            // Created hosted services.
+            services.AddHostedService<MQTTPublishingService>();
+
+            if (cfg.HASS.DiscoveryEnabled)
+                services.AddHostedService<HomeAssistantDiscoveryService>();
+        }
+        else
+        {
+            Console.WriteLine("MQTT Publishing and Discovery services disabled, due to MQTT Configuration missing.");
+        }
+    }
+}
