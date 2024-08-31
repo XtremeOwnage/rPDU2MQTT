884777efeadc88a5fb59e0744c0bd56d72822238
XtremeOwnageDotCom <5262735+XtremeOwnageDotCom@users.noreply.github.com>
2024-08-31 11:13:03 -0500

    Updated helpers to compensate for changes. Removed a few unused extesions. Added a few extension.

--- rPDU2MQTT/Extensions/EntityWithName_Overrides.cs
+++ rPDU2MQTT/Extensions/EntityWithName_Overrides.cs
@@ -1,92 +1,92 @@
 ﻿using rPDU2MQTT.Helpers;
 using rPDU2MQTT.Interfaces;
 using rPDU2MQTT.Models.Config;
-using rPDU2MQTT.Models.PDU.basePDU;
+using rPDU2MQTT.Models.Config.Schemas;
+using rPDU2MQTT.Models.PDU;
 using rPDU2MQTT.Models.PDU.DummyDevices;
+using System.Diagnostics.CodeAnalysis;
 
 namespace rPDU2MQTT.Extensions;
 
 public static class EntityWithName_Overrides
 {
-    /// <summary>
-    /// This method will both apply any overides specified for entity_id, and name. And, it will return if this entity is enabled or not.
-    /// </summary>
-    /// <typeparam name="TEntity"></typeparam>
-    /// <typeparam name="Key"></typeparam>
-    /// <param name="entity"></param>
-    /// <param name="key"></param>
-    /// <param name="Overrides"></param>
-    /// <returns></returns>
-    public static void ApplyOverrides<TEntity>(this TEntity entity, string key, TypeOverride Overrides, string? DefaultName = null, string? DefaultDisplayName = null)
-        where TEntity : NamedEntity
-    {
-        entity.Entity_Name = entity.GetOverrideOrDefault(key, Overrides.ID, FormatAsName: true, DefaultValue: DefaultName);
-        entity.Entity_DisplayName = entity.GetOverrideOrDefault(key, Overrides.Name, FormatAsName: false, DefaultValue: DefaultDisplayName);
-        entity.Entity_Enabled = tryGetValue(Overrides.Enabled, key, out bool enabled, DefaultValue: true) ? enabled : true;
-    }
 
     /// <summary>
-    /// This calculates the name of an entity, based on a collection of overrides.
+    /// Sets the <see cref="NamedEntity.Entity_Name"/>, <see cref="NamedEntity.Entity_DisplayName"/>, and <see cref="BaseEntity.Entity_Enabled"/> properties 
+    /// based on the provided <paramref name="overrides"/> or default functions.
     /// </summary>
-    /// <remarks>
-    /// Suppose you could always do String1 ?? String2 ?? String3 ?? "Default. 
-    /// But- this was funner. also- this checks for empty/whitespace strings.
-    /// </remarks>
-    /// <param name="entity"></param>
-    /// <param name="Key"></param>
-    /// <param name="Overrides"></param>
-    public static string GetOverrideOrDefault<T>(this T entity, string? key, Dictionary<string, string> Overrides, string? DefaultValue = null, bool FormatAsName = false)
-        where T : NamedEntity
+    /// <typeparam name="TKey">The type of the key used in the dictionary of entities.</typeparam>
+    /// <typeparam name="TEntity">The type of the entity in the dictionary, which must be a <see cref="NamedEntity"/>.</typeparam>
+    /// <param name="entities">The dictionary of entities to update.</param>
+    /// <param name="overrides">The overrides object that may contain specific override values for the entities.</param>
+    /// <param name="DefaultNameFunc">A function that provides the default name for an entity based on the key and the entity itself.</param>
+    /// <param name="DefaultDisplayNameFunc">A function that provides the default display name for an entity based on the key and the entity itself. If null, it defaults to <paramref name="DefaultNameFunc"/>.</param>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="DefaultNameFunc"/> is null.</exception>
+    /// <exception cref="Exception">Thrown when unable to determine the entity ID or name, or if any other unexpected error occurs during processing.</exception>
+    public static void SetEntityNameAndEnabled<TKey, TEntity>([DisallowNull] this Dictionary<TKey, TEntity> entities, [DisallowNull] Overrides overrides, [DisallowNull] Func<TKey, TEntity, string> DefaultNameFunc, [DisallowNull] Func<TKey, TEntity, string> DefaultDisplayNameFunc)
+        where TKey : notnull
+        where TEntity : notnull, NamedEntity
     {
-        string formatIfNeeded(string input) => FormatAsName switch
+        if (DefaultNameFunc is null)
+            throw new ArgumentNullException(nameof(DefaultNameFunc));
+
+        DefaultDisplayNameFunc ??= DefaultNameFunc;
+
+        foreach (var (key, entity) in entities)
         {
-            true => input.FormatName(),
-            false => input
-        };
+            EntityOverride? entityOverride = (key, entity) switch
+            {
+                (int k, Outlet o) => overrides.Outlets!.TryGetValue(k, out var outletOverride) ? outletOverride : null,
+                (string k, Device o) => overrides.Devices!.TryGetValue(k, out var outletOverride) ? outletOverride : null,
+                (string k, Measurement o) => overrides.Measurements!.TryGetValue(o.Type, out var outletOverride) ? outletOverride : null,
+                _ => null
+            };
 
-        if (string.IsNullOrEmpty(key))
-            throw new NullReferenceException("Key is null");
-
-        if (Overrides.TryGetValue(key, out string overrideValue))
-            return formatIfNeeded(overrideValue);
-
-        if (!string.IsNullOrEmpty(DefaultValue))
-            return formatIfNeeded(DefaultValue);
-
-        if (entity is EntityWithNameAndLabel entityWithNameAndLabel)
-            return formatIfNeeded(entityWithNameAndLabel.Label ?? entityWithNameAndLabel.Name);
-
-        else
-            throw new Exception("Unable to determine suitable name.");
+            // If overrides are defined, use those.
+            if (entityOverride is not null)
+            {
+                entity.Entity_Name = Coalesce(entityOverride.ID, DefaultNameFunc?.Invoke(key, entity), entity.Entity_Name)?.FormatName() ?? throw new Exception("Unable to determine entity ID.");
+                entity.Entity_DisplayName = Coalesce(entityOverride.Name, DefaultDisplayNameFunc?.Invoke(key, entity), entity.Entity_DisplayName) ?? throw new Exception("Unable to determine entity name.");
+                entity.Entity_Enabled = entityOverride.Enabled; //Always default to enabled.
+            }
+            else // No overrides defined. Set defaults.
+            {
+                entity.Entity_Name = DefaultNameFunc!.Invoke(key, entity);
+                entity.Entity_DisplayName = DefaultDisplayNameFunc!.Invoke(key, entity);
+                entity.Entity_Enabled = true;
+            }
+        }
     }
 
     /// <summary>
-    /// Multi-type lookup. Does case-insensitive compare for strings.
+    /// Prune disabled items from dictionary.
     /// </summary>
     /// <typeparam name="TKey"></typeparam>
-    /// <param name="dictionary"></param>
-    /// <param name="key"></param>
-    /// <param name="Result"></param>
-    /// <returns></returns>
-    private static bool tryGetValue<TKey, TValue>(this Dictionary<TKey, TValue> dictionary, TKey? key, out TValue Result, TValue DefaultValue)
-        where TKey : notnull
+    /// <typeparam name="TEntity"></typeparam>
+    /// <param name="entities"></param>
+    public static void PruneDisabled<TKey, TEntity>([DisallowNull] this Dictionary<TKey, TEntity> entities)
+     where TKey : notnull
+     where TEntity : notnull, NamedEntity
     {
-        if (key is null)
-        {
-            Result = DefaultValue;
-            return false;
-        }
-        if (key is string sKey && dictionary is Dictionary<string, TValue> stringDictionary)
-            return caseInsensitiveLookup<TValue>(stringDictionary, sKey, out Result, DefaultValue);
+        var disabled = entities.Where(o => o.Value.Entity_Enabled == false).ToArray();
 
-        if (dictionary.ContainsKey(key))
-        {
-            Result = dictionary[key];
-            return true;
-        }
+        foreach (var entity in disabled)
+            entities.Remove(entity.Key);
+    }
 
-        Result = DefaultValue;
-        return false;
+    /// <summary>
+    /// Sets <see cref="IMQTTKey.Record_Key"/> for all items in dictionary based on <paramref name="ValueFunc"/>
+    /// </summary>
+    /// <typeparam name="TKey"></typeparam>
+    /// <typeparam name="TEntity"></typeparam>
+    /// <param name="entities"></param>
+    /// <param name="ValueFunc"></param>
+    public static void SetRecordKey<TKey, TEntity>([DisallowNull] this Dictionary<TKey, TEntity> entities, [DisallowNull] Func<TKey, TEntity, string> ValueFunc)
+         where TKey : notnull
+         where TEntity : notnull, NamedEntity
+    {
+        foreach (var (key, entity) in entities)
+            entity.Record_Key = ValueFunc!.Invoke(key, entity);
     }
 
 
