a8b2dc301a96256e3dfaba12134a1679b76b3804
XtremeOwnageDotCom <5262735+XtremeOwnageDotCom@users.noreply.github.com>
2024-08-31 11:11:53 -0500

    Cleaned up logic for setting names, overrides, etc.

--- rPDU2MQTT/Classes/PDU.cs
+++ rPDU2MQTT/Classes/PDU.cs
@@ -44,8 +44,8 @@ public partial class PDU
         data.Record_Parent = null;
         data.Record_Key = config.MQTT.ParentTopic;
 
-        data.Entity_Identifier = Coalesce(config.Overrides.PduID, "rPDU2MQTT")!;
-        data.Entity_Name = data.Entity_DisplayName = Coalesce(config.Overrides.PduName, data.Sys.Label, data.Sys.Name, "rPDU2MQTT")!;
+        data.Entity_Identifier = Coalesce(config.Overrides?.PDU?.ID, "rPDU2MQTT")!;
+        data.Entity_Name = data.Entity_DisplayName = Coalesce(config.Overrides?.PDU?.Name, data.Sys.Label, data.Sys.Name, "rPDU2MQTT")!;
 
         // Propagate down the parent, and identifier.
         data.Devices.SetParentAndIdentifier(data);
@@ -56,14 +56,20 @@ public partial class PDU
 
     private void processDevices(Dictionary<string, Device> devices)
     {
+        //devices.SetEntityNameAndEnabled(config.Overrides, null, null);
         foreach (var (key, device) in devices)
         {
-            // Remove any disabled outlets.
-            config.Outlets.RemoveDisabledRecords(device.Outlets);
-
             // Propagate down the parent, and identifier.
             device.Entity.SetParentAndIdentifier(BaseEntity.FromDevice(device, MqttPath.Entity));
             device.Outlets.SetParentAndIdentifier(BaseEntity.FromDevice(device, MqttPath.Outlets));
+
+            // Set Overrides
+            device.Outlets.SetEntityNameAndEnabled(config.Overrides, DefaultNames.UseEntityName, DefaultNames.UseEntityLabel);
+            device.Entity.SetEntityNameAndEnabled(config.Overrides, DefaultNames.UseEntityName, DefaultNames.UseEntityLabel);
+
+            // Prune disabled items.
+            device.Outlets.PruneDisabled();
+            device.Entity.PruneDisabled();
 
             // Update properties for children.
             processChildDevice(device.Entity);
@@ -74,38 +80,15 @@ public partial class PDU
     {
         foreach (var (key, entity) in entities)
         {
-            if (entity is Outlet o)
-            {
-                int k = int.TryParse(key, out int s) ? s + 1 : 0; //Note- the plus one, is so the number aligns with what is seen on the GUI.
-                entity.ApplyOverrides(k.ToString(), config.Outlets);
-            }
-            else
-            {
-                entity.Entity_Name = (entity.Label ?? entity.Name).FormatName();
-                entity.Entity_DisplayName = (entity.Label ?? entity.Name);
-            }
-
-            // Remove any disabled measurements.
-            config.Measurements.RemoveDisabledRecords(entity.Measurements, o => o.Type);
-
             // All measurements will be stored into a sub-key.
             entity.Measurements.SetParentAndIdentifier(BaseEntity.FromDevice(entity, MqttPath.Measurements));
 
-            // Update properties for measurements.
-            processMeasurements(entity.Measurements);
-        }
-    }
+            // Set Overrides
+            entity.Measurements.SetEntityNameAndEnabled(config.Overrides, DefaultNames.UseMeasurementType, DefaultNames.UseMeasurementType);
+            entity.Measurements.PruneDisabled();
 
-    private void processMeasurements<T>(Dictionary<string, T> measurements) where T : Measurement
-    {
-        foreach (var (key, entity) in measurements)
-        {
-            // We want to override the default key here- to give a nice, readable key.
-            entity.Record_Key = entity.Type;
-            entity.ApplyOverrides(entity.Type, config.Measurements, DefaultName: entity.Type, DefaultDisplayName: entity.Type);
-
-            //SInce- ApplyNameOverridesReturnIsValid already set the EntityName, we are just going to append a suffix to it.
-            entity.Entity_Name = entity.GetEntityName(entity.Entity_Name);
+            // Set MQTT "Key" for measurements, to correspond to measurement type. (EntityName, contains the type)
+            entity.Measurements.SetRecordKey((_, e) => e.Entity_Name);
         }
     }
 }
