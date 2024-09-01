766b0031e6f1ec7980e47f0d1608432a5427c128
XtremeOwnageDotCom <5262735+XtremeOwnageDotCom@users.noreply.github.com>
2024-09-01 13:14:16 -0500

    This fixes #27, caused because the default value was null.

--- rPDU2MQTT/Models/Config/Overrides.cs
+++ rPDU2MQTT/Models/Config/Overrides.cs
@@ -7,14 +7,17 @@ namespace rPDU2MQTT.Models.Config;
 public class Overrides
 {
     [YamlMember(Alias = "PDU", DefaultValuesHandling = DefaultValuesHandling.OmitNull, Description = "Allows overriding values for the PDU itself.")]
-    public EntityOverride? PDU { get; set; }
+    public EntityOverride PDU { get; set; } = new();
+
 
     [YamlMember(Alias = "Devices", DefaultValuesHandling = DefaultValuesHandling.OmitNull, Description = "Allows overriding configuration for individual devices.")]
-    public Dictionary<string, EntityOverride?>? Devices { get; set; }
+    public Dictionary<string, EntityOverride?> Devices { get; set; } = new();
+
 
     [YamlMember(Alias = "Outlets", DefaultValuesHandling = DefaultValuesHandling.OmitNull, Description = "Allows overriding values for individual outlets.")]
-    public Dictionary<int, EntityOverride?>? Outlets { get; set; }
+    public Dictionary<int, EntityOverride?> Outlets { get; set; } = new();
+
 
     [YamlMember(Alias = "Measurements", DefaultValuesHandling = DefaultValuesHandling.OmitNull, Description = "Allows overriding individual measurements")]
-    public Dictionary<string, EntityOverride?>? Measurements { get; set; }
+    public Dictionary<string, EntityOverride?> Measurements { get; set; } = new();
 }
