4758d9e238b533e02bfb871674ac07c2e466190a
XtremeOwnageDotCom <5262735+XtremeOwnageDotCom@users.noreply.github.com>
2024-09-01 14:24:45 -0500

    Added Configuration for Logging.

--- rPDU2MQTT/Models/Config/LoggingConfig.cs
+++ rPDU2MQTT/Models/Config/LoggingConfig.cs
@@ -1,9 +1,27 @@
-﻿namespace rPDU2MQTT.Models.Config;
+﻿using rPDU2MQTT.Models.Config.Schemas;
+using Serilog.Events;
+using YamlDotNet.Serialization;
+
+namespace rPDU2MQTT.Models.Config;
 
 /// <summary>
-/// Configuration settings for Home Assistant integration.
+/// Configuration for logging.
 /// </summary>
 public class LoggingConfig
 {
     public string? LogFilePath { get; set; } = null;
+
+    [YamlMember(Alias = "Console")]
+    public LoggingTargetConfiguration Console { get; set; } = new LoggingTargetConfiguration()
+    {
+        Enabled = true,
+        Severity = LogEventLevel.Information,
+    };
+
+    [YamlMember(Alias = "File")]
+    public LoggingTargetConfiguration File { get; set; } = new LoggingTargetConfiguration()
+    {
+        Enabled = false,
+        Severity = LogEventLevel.Debug
+    };
 }
---
+++ rPDU2MQTT/Models/Config/Schemas/LoggingTargetConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using Serilog.Events;
+
+namespace rPDU2MQTT.Models.Config.Schemas;
+
+public class LoggingTargetConfiguration
+{
+    public LogEventLevel Severity { get; set; } = LogEventLevel.Information;
+
+    public string Format { get; set; } = "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj}{NewLine}{Exception}";
+
+    public bool Enabled { get; set; } = false;
+
+    public string? Path { get; set; } = null;
+}
