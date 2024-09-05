99145ca483f50792a6ff189efa0cba31026eab07
XtremeOwnageDotCom <5262735+XtremeOwnageDotCom@users.noreply.github.com>
2024-09-04 21:26:20 -0500

    Implements #43 - Adds Status topic & Last will message.

---
+++ rPDU2MQTT/Classes/TaskManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Concurrent;
+
+namespace rPDU2MQTT.Classes;
+
+/// <summary>
+/// This class, exists to watch tasks until completion. Non-async code, which needs to execute a task, just passes the task off to this class.
+/// </summary>
+public static class TaskManager
+{
+    private static readonly ConcurrentBag<Task> taskBag = new ConcurrentBag<Task>();
+
+    public static void AddTask(Action task)
+    {
+        Task newTask = Task.Run(task);
+        taskBag.Add(newTask);
+    }
+
+    public static void AddTask(Task task)
+    {
+        taskBag.Add(task);
+    }
+
+    public static void WaitForAllTasks()
+    {
+        foreach (var task in taskBag)
+        {
+            task.Wait();
+        }
+
+        taskBag.Clear();  // ConcurrentBag doesn't have a clear method, but assigning a new one is equivalent to clearing.
+    }
+}
